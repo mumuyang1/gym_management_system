5736373aafed0647f06285ed8fd2a5494c578dc6
mumuyang1 <mumuyang1@outlook.com>
2015-07-28 14:12:59 +0800

    add angular config

---
+++ web/src/main/webapp/angular/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular
+    .module('gym_management_systemApp', [
+        'ngRoute'
+    ])
+
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/users', {
+                templateUrl: './views/employeesManagement.html',
+                controller: 'UsersCtrl'
+            })
+            //
+            //.when('/shoppingMall/:pageNow', {
+            //    templateUrl: 'views/shoppingMall.html',
+            //    controller: 'ShoppingMallCtrl'
+            //})
+            //.when('/cart', {
+            //    templateUrl: 'views/cart.html',
+            //    controller: 'CartCtrl'
+            //})
+            //.when('/pay', {
+            //    templateUrl: 'views/pay.html',
+            //    controller: 'PayCtrl'
+            //})
+            //.when('/productManage', {
+            //    templateUrl: 'views/productManage.html',
+            //    controller: 'ProductManageCtrl'
+            //})
+            //.when('/categoryManage', {
+            //    templateUrl: 'views/categoryManage.html',
+            //    controller: 'CategoryManageCtrl'
+            //})
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
---
+++ web/src/main/webapp/angular/scripts/controllers/user.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('gym_management_systemApp')
+    .controller('UsersCtrl', function ($scope) {
+
+        //$scope.$emit('to-parent-productManageActive');
+        //
+        //
+        //function refresh() {
+        //    categoryManageService.getCategories(function (data) {
+        //        $scope.categories = data;
+        //    });
+        //}
+        //
+        //refresh();
+        //
+        //function showCharge(add, change, clickDelete) {
+        //    $scope.clickAddCategory = add;
+        //    $scope.clickChangeCategory = change;
+        //    $scope.clickDelete = clickDelete;
+        //}
+
+
+
+    });
