69de212d41a26911b8db89e338e02da8c2590192
mumuyang1 <mumuyang1@outlook.com>
2015-08-04 19:33:13 +0800

    implement hibernate with spring

--- core/src/main/java/com/tw/core/dao/hibernateDao/GenericityImpl.java
+++ core/src/main/java/com/tw/core/dao/hibernateDao/GenericityImpl.java
@@ -3,21 +3,32 @@ package com.tw.core.dao.hibernateDao;
 import com.tw.core.utils.HibernateUtil;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
  * Created by yzli on 8/3/15.
  */
 @Repository
+@EnableTransactionManagement
+@Transactional
+
 public class GenericityImpl<T> implements GenericityInterface<T> {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
 
     @Override
     public List<T> getDataList(Class<T> tClass) {
 
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
+        Session session = sessionFactory.getCurrentSession();
+//        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 
         Criteria criteria = session.createCriteria(tClass);
         List<T> list = criteria.list();
@@ -25,7 +36,6 @@ public class GenericityImpl<T> implements GenericityInterface<T> {
 //        String s = tClass.getName().substring(19);
 //        List<T> list = session.createQuery("from " + s).list();
 
-        session.getTransaction().commit();
         return list;
     }
 
--- core/src/test/java/com/tw/core/dao/CourseDaoTest.java
+++ core/src/test/java/com/tw/core/dao/CourseDaoTest.java
@@ -1,7 +1,25 @@
 package com.tw.core.dao;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.transaction.Transactional;
+
 /**
  * Created by yzli on 8/4/15.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/spring-servlet.xml" })
+@Transactional
 public class CourseDaoTest {
+
+    @Test
+    public void test(){
+
+    }
+
+
 }
