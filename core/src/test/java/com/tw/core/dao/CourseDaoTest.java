2c338ddc69ef289193e2e3c67754360b82571a44
mumuyang1 <mumuyang1@outlook.com>
2015-08-06 12:27:42 +0800

    test GenericityImpl all function

--- core/src/test/java/com/tw/core/dao/CourseDaoTest.java
+++ core/src/test/java/com/tw/core/dao/CourseDaoTest.java
@@ -2,47 +2,72 @@ package com.tw.core.dao;
 
 import com.tw.core.dao.hibernateDao.GenericityInterface;
 import com.tw.core.entity.Course;
+import com.tw.core.entity.Employee;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.transaction.Transactional;
-
 import java.util.List;
 
-import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by yzli on 8/4/15.
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath*:src/main/webapp/WEB-INF/spring-servlet.xml")
-//@ContextConfiguration(locations = "classpath*:/spring-servlet.xml")
+@ContextConfiguration(locations = "classpath*:applicationContext.xml")
 @Transactional
+
 public class CourseDaoTest {
 
     @Autowired
     private GenericityInterface<Course> genericityInterface;
 
     @BeforeClass
-    public void setSystemProperty(){
-        System.setProperty("databaseConfig","database-test");
+    public static void setSystemProperty() {
+        System.setProperty("databaseConfig", "database-test");
     }
 
+
     @Test
-    public void test() {
+    public void test_get_data_function() {
 
         List<Course> courseList = genericityInterface.getDataList(Course.class);
 
-        System.out.println(courseList.get(0).getEmployee().getName());
-
-        assertTrue(true);
+        assertEquals("游泳", courseList.get(1).getName());
     }
 
+    @Test
+    public void test_insert_data_function() {
 
+        Course course = new Course(new Employee(2), "滑雪");
+
+        genericityInterface.insertData(course);
+
+        assertEquals("滑雪", genericityInterface.getDataList(Course.class).get(2).getName());
+        assertEquals(3, genericityInterface.getDataList(Course.class).size());
+    }
+
+    @Test
+    public void test_delete_data_function() {
+
+        Course course = new Course(1);
+        genericityInterface.deleteData(course);
+
+        assertEquals(1, genericityInterface.getDataList(Course.class).size());
+    }
+
+    @Test
+    public void test_update_data_function() {
+
+        Course course = new Course(1, "哑铃", new Employee(1));
+        genericityInterface.updateData(course);
+
+        assertEquals("哑铃", genericityInterface.getDataList(Course.class).get(0).getName());
+    }
 }
